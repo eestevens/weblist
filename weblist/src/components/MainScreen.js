de49e4ed1b07a547708b5ec5eed20cc4cb6cf624
eestevens <eedwardstevens@gmail.com>
2018-07-18 20:10:16 -0500

    Refactoring component did mount method and adding filtering option

--- weblist/src/components/MainScreen.js
+++ weblist/src/components/MainScreen.js
@@ -10,10 +10,42 @@ import FilteredList from "./FilteredList";
 class MainScreen extends Component {
   // default state object
   state = {
-    objectItems: []
+    objectItems: [],
+    initialItems : []
   };
 
   componentDidMount() {
+    console.log("did mount");
+    this.getData();
+    this.sortData();
+  }
+
+  sortData() {
+    console.log("sort data");
+    const unSortedItems = this.state.initialItems;
+    const allItems = this.state.initialItems;
+    unSortedItems.sort(function(a, b){
+      var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase();
+      if (nameA < nameB) //sort string ascending
+      return -1;
+      if (nameA > nameB)
+      return 1;
+      return 0; //default return value (no sorting)
+    });
+    // create a new "state" object without mutating
+    // the original state object.
+    const newState = Object.assign({}, this.state, {
+      objectItems: unSortedItems,
+      initialItems: allItems
+    });
+
+    // store the new state object in the component's state
+    this.setState(newState);
+
+  }
+
+  getData() {
+    console.log("get data");
     axios
       .get("https://jsonplaceholder.typicode.com/users")
       .then(response => {
@@ -26,36 +58,38 @@ class MainScreen extends Component {
             email : c.email
           };
         });
-
-        newObjectItems.sort(function(a, b){
-          var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase();
-          if (nameA < nameB) //sort string ascending
-          return -1;
-          if (nameA > nameB)
-          return 1;
-          return 0; //default return value (no sorting)
-        });
-
-        // create a new "state" object without mutating
-        // the original state object.
         const newState = Object.assign({}, this.state, {
-          objectItems: newObjectItems
+          objectItems: newObjectItems,
+          initialItems : newObjectItems
         });
-
-        // store the new state object in the component's state
         this.setState(newState);
       })
       .catch(error => console.log(error));
   }
+  filterList(event) {
+    var updatedList = this.state.initialItems;
+    console.log("Filter");
+    console.log(this.state.objectItems.length);
+    updatedList = updatedList.filter(function (item) {
+      return item.name.toLowerCase().search(event.target.value.toLowerCase()) !== -1;
+    });
+    console.log(this.state.initialItems);
+    console.log(updatedList.length);
+    this.setState({ objectItems: updatedList, initialItems : this.state.initialItems });
+  }
 
   render() {
+    console.log("render");
+    console.log(this.state.initialItems.length);
+    console.log(this.state.objectItems.length);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">React Contact Manager</h1>
         </header>
-        <FilteredList/>
+
+        <input type="text" placeholder="Search" onChange={this.filterList.bind(this)}/>
         <ObjectList objectItems={this.state.objectItems} />
       </div>
     );

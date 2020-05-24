c8c64967071b9f543c3b2935e8a671474ba6efdd
Erik Stevens <erikstevens@northwesternmutual.com>
2020-05-23 21:07:44 -0500

    Updates

---
+++ precache-manifest.36727b01d9b8e5f311ce08ab0bbe3d01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3f3db7861d42770c56a1522fef67d8",
+    "url": "/magicGathering/index.html"
+  },
+  {
+    "revision": "73e333ecf36410d94349",
+    "url": "/magicGathering/static/css/main.1064839b.chunk.css"
+  },
+  {
+    "revision": "1eb1f9f01479fdf792d9",
+    "url": "/magicGathering/static/js/2.3b14c150.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/magicGathering/static/js/2.3b14c150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e333ecf36410d94349",
+    "url": "/magicGathering/static/js/main.22d39cb5.chunk.js"
+  },
+  {
+    "revision": "63de7c3843348c3fcb4f",
+    "url": "/magicGathering/static/js/runtime-main.ae2e2553.js"
+  },
+  {
+    "revision": "ebc77ba3574e86fa02a171ae63776bd2",
+    "url": "/magicGathering/static/media/mtg-logo.ebc77ba3.jpg"
+  }
+]);
